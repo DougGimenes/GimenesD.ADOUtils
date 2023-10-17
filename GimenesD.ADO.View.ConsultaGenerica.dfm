b68f1443e5b43af56cfcb0dbbb9b9020074cb62c
DouguiGimenes <douglas.gimenes99@gmail.com>
2023-10-17 13:25:47 -0300

    Separação dos utilitários em um repositório simplificado

---
+++ GimenesD.ADO.View.ConsultaGenerica.dfm
@@ -0,0 +1,81 @@
+object FrmConsultaBase: TFrmConsultaBase
+  Left = 0
+  Top = 0
+  Caption = 'Consulta'
+  ClientHeight = 352
+  ClientWidth = 648
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -16
+  Font.Name = 'Arial'
+  Font.Style = []
+  OldCreateOrder = True
+  Position = poMainFormCenter
+  PixelsPerInch = 96
+  TextHeight = 18
+  object PnlMain: TPanel
+    Left = 0
+    Top = 0
+    Width = 648
+    Height = 352
+    Align = alClient
+    BevelOuter = bvNone
+    Color = clWhite
+    ParentBackground = False
+    TabOrder = 0
+    DesignSize = (
+      648
+      352)
+    object DbgConsulta: TDBGrid
+      Left = 10
+      Top = 56
+      Width = 627
+      Height = 277
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      DataSource = DsConsulta
+      Options = [dgTitles, dgColumnResize, dgColLines, dgRowLines, dgTabs, dgRowSelect, dgConfirmDelete, dgCancelOnExit, dgTitleClick, dgTitleHotTrack]
+      TabOrder = 0
+      TitleFont.Charset = DEFAULT_CHARSET
+      TitleFont.Color = clWindowText
+      TitleFont.Height = -16
+      TitleFont.Name = 'Arial'
+      TitleFont.Style = []
+      OnDblClick = DbgConsultaDblClick
+      OnKeyDown = DbgConsultaKeyDown
+    end
+    object EdtConsulta: TEdit
+      Left = 10
+      Top = 12
+      Width = 627
+      Height = 26
+      Anchors = [akLeft, akTop, akRight]
+      TabOrder = 1
+      TextHint = 'Consulta'
+      OnChange = EdtConsultaChange
+      OnKeyDown = EdtConsultaKeyDown
+    end
+  end
+  object QryConsulta: TADOQuery
+    Parameters = <>
+    Left = 40
+    Top = 128
+  end
+  object DsConsulta: TDataSource
+    DataSet = QryConsulta
+    Left = 40
+    Top = 176
+  end
+  object TimConsulta: TTimer
+    Enabled = False
+    Interval = 3000
+    OnTimer = TimConsultaTimer
+    Left = 40
+    Top = 224
+  end
+  object QryAutoComplete: TADOQuery
+    Parameters = <>
+    Left = 120
+    Top = 128
+  end
+end
---
+++ GimenesD.ADO.View.ConsultaGenerica.pas
@@ -0,0 +1,163 @@
+unit GimenesD.ADO.View.ConsultaGenerica;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.ExtCtrls,
+  Data.DB,
+  Vcl.StdCtrls,
+  Vcl.Grids,
+  Vcl.DBGrids,
+  Data.Win.ADODB;
+
+type
+  TFrmConsultaBase = class(TForm)
+    DbgConsulta: TDBGrid;
+    EdtConsulta: TEdit;
+    QryConsulta: TADOQuery;
+    DsConsulta: TDataSource;
+    TimConsulta: TTimer;
+    QryAutoComplete: TADOQuery;
+    PnlMain: TPanel;
+    procedure DbgConsultaDblClick(Sender: TObject);
+    procedure DbgConsultaKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+    procedure TimConsultaTimer(Sender: TObject);
+    procedure EdtConsultaChange(Sender: TObject);
+    procedure EdtConsultaKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+  protected
+    { Private declarations }
+    FCodigoConsultado: Integer;
+    FNomeConsultado: String;
+    FObjetoConsultado: TObject;
+    FConsultou: Boolean;
+    Apagando: Boolean;
+    Conexao: TADOConnection;
+
+    procedure FinalizarConsulta(); virtual; abstract;
+    function  FormarFiltro() : String; virtual; abstract;
+    function  FormarSQLAutoComplete() : String; virtual; abstract;
+    procedure Filtrar();
+  public
+    { Public declarations }
+    property ObjetoConsultado : TObject read FObjetoConsultado;
+    property CodigoConsultado: Integer read FCodigoConsultado;
+    property NomeConsultado: String read FNomeConsultado;
+    property Consultou : Boolean read FConsultou;
+
+    constructor Create(AOwner: TComponent; AConexao: TADOConnection); overload;
+  end;
+
+var
+  FrmConsultaBase: TFrmConsultaBase;
+
+implementation
+
+{$R *.dfm}
+
+{ TFrmConsultaBase }
+
+constructor TFrmConsultaBase.Create(AOwner: TComponent; AConexao: TADOConnection);
+begin
+  inherited Create(AOwner);
+  Self.Conexao := AConexao;
+
+  Self.QryConsulta.Connection := AConexao;
+  Self.QryAutoComplete.Connection := AConexao;
+end;
+
+procedure TFrmConsultaBase.DbgConsultaDblClick(Sender: TObject);
+begin
+  inherited;
+  Self.FinalizarConsulta();
+  Self.Close();
+end;
+
+procedure TFrmConsultaBase.DbgConsultaKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  inherited;
+  if Key = VK_Return then
+  begin
+    Self.FinalizarConsulta();
+    Self.Close();
+  end;
+end;
+
+procedure TFrmConsultaBase.EdtConsultaChange(Sender: TObject);
+var
+  Inicial : Integer;
+begin
+  inherited;
+  if not Self.Apagando then
+  begin
+    Inicial := Length(Self.EdtConsulta.Text);
+    Self.QryAutoComplete.Close();
+    Self.QryAutoComplete.SQL.Text := Self.FormarSQLAutoComplete();
+    Self.QryAutoComplete.Open();
+
+    Self.EdtConsulta.Text := Self.EdtConsulta.Text + Self.QryAutoComplete.Fields[0].AsString.Substring(Inicial);
+    Self.EdtConsulta.SelStart := Inicial;
+    Self.EdtConsulta.SelLength := Length(Self.EdtConsulta.Text) - Inicial;
+  end;
+
+  Self.TimConsulta.Enabled := False;
+
+  if Inicial >= 3 then
+  begin
+    Self.TimConsulta.Interval := 1000;
+    Self.TimConsulta.Enabled  := True;
+  end;
+end;
+
+procedure TFrmConsultaBase.EdtConsultaKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  inherited;
+  Self.Apagando := False;
+
+  if (Key = VK_BACK) or (Key = VK_DELETE) then
+  begin
+    Self.Apagando := True;
+  end
+  else if Key = VK_Return then
+  begin
+    Self.Filtrar();
+  end;
+end;
+
+procedure TFrmConsultaBase.Filtrar;
+begin
+  if not Self.QryConsulta.Active then
+  begin
+    Self.QryConsulta.Open();
+  end;
+
+  Self.QryConsulta.DisableControls();
+  Self.QryConsulta.Filtered := False;
+  Self.QryConsulta.Filter := Self.FormarFiltro();
+  Self.QryConsulta.Filtered := True;
+  Self.QryConsulta.EnableControls();
+end;
+
+procedure TFrmConsultaBase.TimConsultaTimer(Sender: TObject);
+begin
+  inherited;
+  if Self.EdtConsulta.Text <> '' then
+  begin
+    Self.TimConsulta.Enabled := False;
+    Self.Filtrar();
+  end;
+end;
+
+end.
